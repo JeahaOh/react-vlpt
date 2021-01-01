637b60f70a27d7ce34fd0ba05becac4d4e414fe0
JeahaOh <jeaha1217@gmail.com>
2021-01-01 20:48:31 +0900

    .

--- 01_basic/15_update_ele_on_array/src/App.js
+++ 01_basic/15_update_ele_on_array/src/App.js
@@ -3,17 +3,17 @@ import UserList from './UserList';
 import CreateUser from './CreateUser';
 
 function App() {
-  const [inputs, setInputs ] = useState({
-    username : '',
-    email : '',
+  const [inputs, setInputs] = useState({
+    username: '',
+    email: '',
   });
 
   const { username, email } = inputs;
   const onChange = e => {
-    const {name, value } = e.target;
+    const { name, value } = e.target;
     setInputs({
       ...inputs,
-      [name] : value
+      [name]: value
     });
   }
 
@@ -41,29 +41,29 @@ function App() {
   const nextId = useRef(4);
 
   const onCreate = () => {
-    console.log( nextId.current );
-    
+    console.log(nextId.current);
+
     const user = {
-      id : nextId.current,
+      id: nextId.current,
       username,
       email,
     };
 
     //  push는 사용하면 안됨.
     // setUsers([...users, user]);
-    setUsers( users.concat(user) );
+    setUsers(users.concat(user));
 
     nextId.current += 1;
 
     setInputs({
-      username : '',
-      email : ''
+      username: '',
+      email: ''
     })
   };
 
   const onRemove = id => {
-    setUsers( users.filter( user => user.id !== id ) );
-    
+    setUsers(users.filter(user => user.id !== id));
+
   }
 
   const onToggle = id => {
@@ -72,8 +72,7 @@ function App() {
         user => user.id === id
           ? { ...user, active: !user.active }
           : user
-
-    ))
+      ));
   }
 
   return (
@@ -84,7 +83,7 @@ function App() {
         onChange={onChange}
         onCreate={onCreate}
       />
-      <UserList users={users} onRemove={onRemove} onToggle={onToggle}/>
+      <UserList users={users} onRemove={onRemove} onToggle={onToggle} />
     </>
   );
 }
